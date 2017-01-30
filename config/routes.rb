691e177de3c379b8e91590764454d858a5dd996c
Tyler <tcameronsmith@gmail.com>
2017-01-30 11:44:02 -0500

    add routes for differnt pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'users#index'
 
+  # get '/users', to: 'users#index'
+    resources :users, only: [:index, :new, :create, :show, :edit, :update]
+  	get '/about', to: 'users#about'
+  	get '/rates', to: 'users#rates'
+
+    resource :sessions, only: [:new, :create, :destroy]
 end
