96da0b32a8eb7500b60444e4a92f85b31bee7a4e
Tyler <tcameronsmith@gmail.com>
2017-02-06 16:47:05 -0500

    add mail method

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,4 +1,9 @@
 class UserMailer < ApplicationMailer
   default from: 'tcameronsmith@gmail.com'
   layout 'mailer'
+
+  def mail_method(data,email,subject)
+    @body=data
+    mail(to: email, subject: subject)
+  end
 end
